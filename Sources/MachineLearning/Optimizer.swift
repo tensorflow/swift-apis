e391e007618632c8e4037dbe848e7381cfdd01a7
Dan Zheng <danielzheng@google.com>
2019-01-14 02:27:02 -0800

    Implement Adam optimizer.
    Implement keypath-based Adam optimizer.

--- Sources/MachineLearning/Optimizer.swift
+++ Sources/MachineLearning/Optimizer.swift
@@ -66,6 +66,60 @@ public class SGDOptimizer<Model : Layer, Scalar: BinaryFloatingPoint & TensorFlo
     }
 }
 
+public class Adam<Model : Layer, Scalar: BinaryFloatingPoint & TensorFlowScalar>: Optimizer
+    where Model.AllDifferentiableVariables : KeyPathIterable & VectorNumeric,
+          Model.AllDifferentiableVariables == Model.CotangentVector,
+          Model.AllDifferentiableVariables.Scalar == Scalar
+{
+    public let learningRate: Scalar
+    public var beta1: Scalar
+    public var beta2: Scalar
+    public let epsilon: Scalar
+    public let decay: Scalar
+
+    public init(
+        learningRate: Scalar = 1e-3,
+        beta1: Scalar = 0.9,
+        beta2: Scalar = 0.999,
+        epsilon: Scalar = 1e-8,
+        decay: Scalar = 0
+    ) {
+        precondition(learningRate >= 0, "Learning rate must be non-negative")
+        precondition(0 <= beta1 && beta1 <= 1, "Beta parameter must be between 0 and 1")
+        precondition(0 <= beta2 && beta2 <= 1, "Beta parameter must be between 0 and 1")
+        precondition(decay >= 0, "Weight decay must be non-negative")
+
+        self.learningRate = learningRate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.decay = decay
+    }
+
+    var step: Scalar = 0
+    var firstMoments = Model.AllDifferentiableVariables.zero
+    var secondMoments = Model.AllDifferentiableVariables.zero
+
+    public func fit(_ model: inout Model, along gradients: Model.CotangentVector) {
+        for kp in model.allDifferentiableVariables
+                       .recursivelyAllWritableKeyPaths(to: Tensor<Scalar>.self) {
+            firstMoments[keyPath: kp] =
+                firstMoments[keyPath: kp] * beta1 + (1 - beta1) * gradients[keyPath: kp]
+            secondMoments[keyPath: kp] =
+                firstMoments[keyPath: kp] * beta2 + (1 - beta2) *
+                gradients[keyPath: kp] * gradients[keyPath: kp]
+
+            let denominator = sqrt(secondMoments[keyPath: kp]) + epsilon
+            step += 1
+            let biasCorrection1 = 1 - pow(beta1, step)
+            let biasCorrection2 = 1 - pow(beta2, step)
+            let stepSize = learningRate * sqrt(biasCorrection2) / biasCorrection1
+            model.allDifferentiableVariables[keyPath: kp] -=
+                stepSize * firstMoments[keyPath: kp] / denominator
+        }
+    }
+}
+
 public class RiemannSGD<Model: Layer, Scalar: FloatingPoint>: Optimizer
     where Model.CotangentVector : VectorNumeric, Model.CotangentVector.Scalar == Scalar {
     public var learningRate: Scalar
