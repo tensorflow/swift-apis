5cc167132c55410d07b42a1a63c6d486df888628
Pruthivi Raj <pruthiviraj71@gmail.com>
2019-03-15 01:36:12 +0530

    Added Native Swift implementation except for downloading and Extracting gz files and moved the script to Sources/DeepLearningContrib.

--- Sources/DeepLearningContrib/MNISTdataset.swift
+++ Sources/DeepLearningContrib/MNISTdataset.swift
@@ -1,11 +1,5 @@
-import Python
+import Foundation
 import TensorFlow
-
-fileprivate let np = Python.import("numpy")
-fileprivate let path = Python.import("os.path")
-fileprivate let urllib = Python.import("urllib.request")
-fileprivate let gzip = Python.import("gzip")
-fileprivate let sh = Python.import("shutil")
 
 /// MNIST constants
 fileprivate enum constants {
@@ -31,43 +25,39 @@ fileprivate func downloadAndExtract() {
                           constants.testImages,
                           constants.testLabels]
     var counter = 0
-    var fin: PythonObject
-    var fout: PythonObject
+    let path = FileManager.default.currentDirectoryPath
     for helperFile in helperFiles {
-        if !Bool(path.isfile(helperFile))! {
-            print("Downloading \(helperFile)")
-            urllib.urlretrieve(constants.URL + helperFile, filename: helperFile)
-            print("Downloaded \(helperFile)")
-        } else {
-            print("Not downloading \(helperFile): already exists")
-        }
+        let url = NSURL(fileURLWithPath: path)
+        if let pathComponent = url.appendingPathComponent(helperFile) {
+            let filePath = pathComponent.path
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: filePath) {
+                print("Not downloading \(helperFile): already exists")
+            } else {
+                print("Downloading \(helperFile)")
+                /// TODO: Add Swift Implementation for downloading files.
+                print("Downloaded \(helperFile)")
+            }
+        }    
     }
     for extractedFile in extractedFiles {
-        if !Bool(path.isfile(extractedFile))! {
-            print("Extracting \(extractedFile)")
-            fin = gzip.open(helperFiles[counter], "rb")
-            fout = Python.open(extractedFiles[counter], "wb")
-            sh.copyfileobj(fin, fout)
-            print("Extraction Completed: \(extractedFile)")
-        } else {
-            print("File \(extractedFile): already exists")
-        }
-        counter = counter + 1
+        let url = NSURL(fileURLWithPath: path)
+        if let pathComponent = url.appendingPathComponent(extractedFile) {
+            let filePath = pathComponent.path
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: filePath) {
+                print("File \(extractedFile): already exists")
+            } else {
+                print("Extracting \(extractedFile)")
+                /// TODO: Add Swift Implementation for extracting downloaded files.
+                print("Extraction Completed: \(extractedFile)")
+            }
+            counter = counter + 1
+        }    
     }
-
 }
 
-/// Reads a file into an array of bytes.
-///
-/// - Parameters:
-///   - filename: A String with file name that is to be converted to array of bytes. 
-/// - Returns: An Array of type UInt8.
-fileprivate func readFile(_ filename: String) -> [UInt8] {
-    let buffer = Python.open(filename, "rb").read()
-    return Array(numpy: np.frombuffer(buffer, dtype: np.uint8))!
-}
-
-/// Used to read MNIST Dataset.
+/// Function to read MNIST Dataset.
 ///
 /// - Returns: A Tuple of 4 Tensors for training images, training labels, test images,
 /// test labels in that order.
@@ -79,10 +69,10 @@ func readMNIST() -> (
 ) {
     downloadAndExtract()
     print("Reading data.")
-    let trainImages = readFile(constants.trainImages).dropFirst(16).map { Float($0) }
-    let trainLabels = readFile(constants.trainLabels).dropFirst(8).map { Int32($0) }
-    let testImages = readFile(constants.testImages).dropFirst(16).map { Float($0) }
-    let testLabels = readFile(constants.testLabels).dropFirst(8).map { Int32($0) }
+    let trainImages = try! Data(contentsOf: URL(fileURLWithPath: constants.trainImages)).dropFirst(16).map { Float($0) }
+    let trainLabels = try! Data(contentsOf: URL(fileURLWithPath: constants.trainLabels)).dropFirst(8).map { Int32($0) }
+    let testImages = try! Data(contentsOf: URL(fileURLWithPath: constants.testImages)).dropFirst(16).map { Float($0) }
+    let testLabels = try! Data(contentsOf: URL(fileURLWithPath: constants.testLabels)).dropFirst(8).map { Int32($0) }
     let trainrowCount = Int32(trainLabels.count)
     let testrowCount = Int32(testLabels.count)
     let columnCount = Int32(testImages.count) / testrowCount
@@ -94,5 +84,3 @@ func readMNIST() -> (
         testLabels: Tensor(testLabels)
     )
 }
-
-let (trainX, trainY, testX, testY) = readMNIST()
