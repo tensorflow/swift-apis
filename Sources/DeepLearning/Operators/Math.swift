9885f38a7f814982481639f6ecfdb47d808c91ac
Anthony Platanios <e.a.platanios@gmail.com>
2019-04-01 09:58:29 -0400

    Re-organized the operators source files.

---
+++ Sources/DeepLearning/Operators/Math.swift
@@ -0,0 +1,22 @@
+// Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if !COMPILING_TENSORFLOW_MODULE
+@_exported import TensorFlow
+#endif
+
+/// Returns the values of the specified tensor rounded to the nearest integer, element-wise.
+public func round<Scalar: BinaryFloatingPoint>(_ x: Tensor<Scalar>) -> Tensor<Scalar> {
+  return Raw.round(x)
+}
