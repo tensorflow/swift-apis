290164f4f2cd12cb9fda32c529a8d3f6921c120b
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2019-04-24 08:58:22 +0530

    Review changes

--- Sources/DeepLearning/Layer.swift
+++ Sources/DeepLearning/Layer.swift
@@ -1188,12 +1188,13 @@ public struct UpSampling3D<Scalar: TensorFlowFloatingPoint>: Layer {
     @differentiable
     public func call(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
         let shape = input.shape
-        let (batchSize, height, width, breadth, channels) = (shape[0], shape[1], shape[2]
-                                                            , shape[3], shape[4])
+        let (batchSize, height, width, depth, channels) =
+                                      (shape[0], shape[1], shape[2], shape[3], shape[4])
         let scaleOnes = Tensor<Scalar>(ones: [1, 1, size, 1, size, 1, size, 1])
-        let upSampling = input.reshaped(to: [batchSize, height, 1, width, 1,
-                                            breadth, 1, channels]) * scaleOnes
-        return upSampling.reshaped(to: [batchSize, height * size, width * size, breadth * size, channels])
+        let upSampling = input.reshaped(
+        to: [batchSize, height, 1, width, 1, depth, 1, channels]) * scaleOnes
+        return upSampling.reshaped(
+        to: [batchSize, height * size, width * size, depth * size, channels])
     }
 }
 
