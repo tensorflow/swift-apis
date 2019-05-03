2e287fab5b4670d0f35ca94de9cb2b53bbb8f5ae
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-03 00:41:08 -0400

    Made the utility and runtime types and functions internal.

--- Sources/TensorFlow/Core/Utilities.swift
+++ Sources/TensorFlow/Core/Utilities.swift
@@ -27,8 +27,8 @@ import CTensorFlow
 /// These checks run in both debug and release modes (while assert() only runs in debug mode), to 
 /// help shake out more bugs and facilitate debugging in the early project phases. It can be
 /// replaced with plain assert() later, when we have a more mature code base.
-@inlinable
-public func internalConsistencyCheck(
+@usableFromInline
+internal func internalConsistencyCheck(
     _ predicate: Bool,
     _ errMessage: String = "TF runtime assertion failure",
     file: StaticString = #file,
@@ -39,8 +39,8 @@ public func internalConsistencyCheck(
     }
 }
 
-@inlinable
-public func checkOk(
+@usableFromInline
+internal func checkOk(
     _ s: CTFStatus?,
     file: StaticString = #file,
     line: UInt = #line
@@ -60,61 +60,73 @@ public func checkOk(
 // the pointer is not NULL.
 
 /// The `TF_Session *` type.
-public typealias CTFSession = OpaquePointer
+@usableFromInline
+internal typealias CTFSession = OpaquePointer
 
 /// The `TF_Status *` type.
-public typealias CTFStatus = OpaquePointer
+@usableFromInline
+internal typealias CTFStatus = OpaquePointer
 
 /// The `TF_Graph*` type.
-public typealias CTFGraph = OpaquePointer
+@usableFromInline
+internal typealias CTFGraph = OpaquePointer
 
 /// The `TF_Function*` type.
-public typealias CTFFunction = OpaquePointer
+@usableFromInline
+internal typealias CTFFunction = OpaquePointer
 
 /// The `TF_Tensor *` type.
-public typealias CTensor = OpaquePointer
+@usableFromInline
+internal typealias CTensor = OpaquePointer
 
 /// The `TF_TensorHandle *` type.
 ///
 /// - Note: This is public so that compiler generated code can read/write tensor handles when
 ///   calling runtime APIs.
-public typealias CTensorHandle = OpaquePointer
+@usableFromInline
+internal typealias CTensorHandle = OpaquePointer
 
 /// The `TFE_Context *` type.
-public typealias CTFEContext = OpaquePointer
+@usableFromInline
+internal typealias CTFEContext = OpaquePointer
 
 /// The `TFE_Op *` type.
-public typealias CTFEOp = OpaquePointer
+@usableFromInline
+internal typealias CTFEOp = OpaquePointer
 
 /// The `TF_OperationDescription *` type.
-public typealias CTFOperationDescription = OpaquePointer
+@usableFromInline
+internal typealias CTFOperationDescription = OpaquePointer
 
 /// The `TFE_TraceContext *` type.
-public typealias CTFETraceContext = OpaquePointer
+@usableFromInline
+internal typealias CTFETraceContext = OpaquePointer
 
 //===------------------------------------------------------------------------------------------===//
 // Logging
 //===------------------------------------------------------------------------------------------===//
 
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-@usableFromInline let stderr = __stderrp
-@usableFromInline let stdout = __stdoutp
+@usableFromInline internal let stderr = __stderrp
+@usableFromInline internal  let stdout = __stdoutp
 #endif
 
 /// Log to standard error.
-public func logToStderr(_ message: StaticString) {
+@usableFromInline
+internal func logToStderr(_ message: StaticString) {
     message.utf8Start.withMemoryRebound(to: Int8.self, capacity: message.utf8CodeUnitCount) {
         _ = fputs($0, stderr)
     }
 }
 
 /// Log to standard error.
-public func logToStderr(_ message: String) {
+@usableFromInline
+internal func logToStderr(_ message: String) {
     _ = fputs(message, stderr)
 }
 
-@inlinable
-public func debugLog(
+@usableFromInline
+internal func debugLog(
     _ message: @autoclosure () -> String,
     file: StaticString = #file,
     line: UInt = #line
@@ -131,7 +143,8 @@ public func debugLog(
 //===------------------------------------------------------------------------------------------===//
 
 /// Given the address of a `TF_Buffer` and a file path, write the buffer's contents to the file.
-public func writeContents(of buffer: UnsafePointer<TF_Buffer>, toFile path: String) {
+@usableFromInline
+internal func writeContents(of buffer: UnsafePointer<TF_Buffer>, toFile path: String) {
     let fp = fopen(path, "w+")
     fwrite(buffer.pointee.data, /*size*/ 1, /*count*/ buffer.pointee.length, fp)
     fclose(fp)
@@ -145,33 +158,33 @@ public func writeContents(of buffer: UnsafePointer<TF_Buffer>, toFile path: Stri
 
 /// This is a generic host-only op that hides the details of its impl in the SIL code. This makes 
 /// reading/writing SIL based compiler unit tests simple.
-@inline(never)
-public func _hostOp<T>(_ x: T) {
+@usableFromInline @inline(never)
+internal func _hostOp<T>(_ x: T) {
     print(x)
 }
 
-@inline(never)
-public func _hostOp<Scalar>(_ x: Tensor<Scalar>) {
+@usableFromInline @inline(never)
+internal func _hostOp<Scalar>(_ x: Tensor<Scalar>) {
     print(x)
 }
 
-@inline(never)
-public func _hostOp<Scalar : TensorFlowScalar>(_ x: TensorHandle<Scalar>) {
+@usableFromInline @inline(never)
+internal func _hostOp<Scalar : TensorFlowScalar>(_ x: TensorHandle<Scalar>) {
     print(Tensor(handle: x))
 }
 
 /// Some TPU ops (e.g. infeed/outfeed) require tensor shape info, which the APIs below can provide.
 ///
 /// TODO: Remove these helper APIs, when we have a better shape inference/propagation design.
-@inlinable @inline(__always)
-public func _scalarTensorWithShape<Scalar: TensorFlowScalar>(
+@usableFromInline @inline(__always)
+internal func _scalarTensorWithShape<Scalar: TensorFlowScalar>(
     _ x: Tensor<Scalar>
 ) -> Tensor<Scalar> {
     return Raw.identity(x)
 }
 
-@inlinable @inline(__always)
-public func _addScalarTensorsWithShape<Scalar: TensorFlowNumeric>(
+@usableFromInline @inline(__always)
+internal func _addScalarTensorsWithShape<Scalar: TensorFlowNumeric>(
     _ x: Tensor<Scalar>,
     _ y: Tensor<Scalar>
 ) -> Tensor<Scalar> {
