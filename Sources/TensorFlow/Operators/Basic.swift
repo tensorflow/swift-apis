1190edb77ca4d56595c4c242393f1897891ab1e6
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-11 16:55:36 -0400

    Merged upstream changes.

--- Sources/TensorFlow/Operators/Basic.swift
+++ Sources/TensorFlow/Operators/Basic.swift
@@ -659,7 +659,7 @@ internal extension Tensor.IndexPath {
     @inlinable
     init(_ ranges: [TensorRange]) {
         precondition(!ranges.isEmpty, "The tensor range collection cannot be empty.")
-        precondition(ranges.count { $0 == TensorRange.ellipsis } < 2,
+        precondition(ranges.lazy.filter { $0 == TensorRange.ellipsis }.count < 2,
                      "Only one ellipsis is allowed per tensor range collection.")
 
         var begin = [Int32](repeating: 0, count: ranges.count)
