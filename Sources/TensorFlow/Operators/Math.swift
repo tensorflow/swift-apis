bd27b8edc84825969a6adffeb4c8e3d58c91b50d
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-22 11:29:58 -0400

    [TF] Fix 'Tensor.replacing(_:_:)' wrong semantics.

--- Sources/TensorFlow/Operators/Math.swift
+++ Sources/TensorFlow/Operators/Math.swift
@@ -825,7 +825,7 @@ public extension Tensor {
     @inlinable
     @differentiable(wrt: (self, other), vjp: _vjpReplacing where Scalar: TensorFlowFloatingPoint)
     func replacing(with other: Tensor, where mask: Tensor<Bool>) -> Tensor {
-        return Raw.select(condition: mask, t: self, e: other)
+        return Raw.select(condition: mask, t: other, e: self)
     }
 }
 
