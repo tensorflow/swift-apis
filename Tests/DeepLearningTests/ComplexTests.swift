e58f4003c9c9a0ccca9495e08cf520d7f288cc4e
Bart Chrzaszcz <bart.chr@gmail.com>
2019-05-23 16:14:04 -0700

    Fix tests for running with XCTest.

--- Tests/DeepLearningTests/ComplexTests.swift
+++ Tests/DeepLearningTests/ComplexTests.swift
@@ -3,147 +3,231 @@
 //
 // Complex API tests.
 
-// TODO: remove import
-import TensorFlow
+import XCTest
+@testable import DeepLearning
 
-import StdlibUnittest
+final class ComplexTests: XCTestCase {
+  func testInitializer() {
+    let complex = Complex<Float>(real: 2, imaginary: 3)
+    XCTAssertEqual(complex.real, 2)
+    XCTAssertEqual(complex.imaginary, 3)
+  }
 
-var ComplexTests = TestSuite("Complex")
+  func testStaticImaginary() {
+    let imaginary = Complex<Float>(real: 0, imaginary: 1)
+    XCTAssertEqual(imaginary, Complex.i)
+  }
 
-ComplexTests.test("Initializer") {
-  let complex = Complex<Float>(real: 2, imaginary: 3)
-  expectEqual(complex.real, 2)
-  expectEqual(complex.imaginary, 3)
+  func testIsFinite() {
+    var complex = Complex<Float>(real: 999, imaginary: 0)
+    XCTAssertTrue(complex.isFinite)
+
+    complex = Complex(real: 1.0 / 0.0, imaginary: 1)
+    XCTAssertFalse(complex.isFinite)
+
+    complex = Complex(real: 1.0 / 0.0, imaginary: 1.0 / 0.0)
+    XCTAssertFalse(complex.isFinite)
+  }
+
+  func testIsInfinite() {
+    var complex = Complex<Float>(real: 999, imaginary: 0)
+    XCTAssertFalse(complex.isInfinite)
+
+    complex = Complex(real: 1.0 / 0.0, imaginary: 1)
+    XCTAssertTrue(complex.isInfinite)
+
+    complex = Complex(real: 1.0 / 0.0, imaginary: 1.0 / 0.0)
+    XCTAssertTrue(complex.isInfinite)
+  }
+
+  func testIsNaN() {
+    var complex = Complex<Float>(real: 999, imaginary: 0)
+    XCTAssertFalse(complex.isNaN)
+
+    complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 1)
+    XCTAssertTrue(complex.isNaN)
+
+    complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0.0 * 1.0 / 0.0)
+    XCTAssertTrue(complex.isNaN)
+  }
+
+  func testIsZero() {
+    var complex = Complex<Float>(real: 999, imaginary: 0)
+    XCTAssertFalse(complex.isZero)
+
+    complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0)
+    XCTAssertFalse(complex.isZero)
+
+    complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0.0 * 1.0 / 0.0)
+    XCTAssertFalse(complex.isZero)
+
+    complex = Complex(real: 0, imaginary: 0)
+    XCTAssertTrue(complex.isZero)
+  }
+
+  func testEquals() {
+    var complexA = Complex<Float>(real: 999, imaginary: 0)
+    let complexB = Complex<Float>(real: 999, imaginary: 0)
+    XCTAssertEqual(complexA, complexB)
+
+    complexA = Complex(real: 5, imaginary: 0)
+    expectNotEqual(complexA, complexB)
+  }
+
+  func testPlus() {
+    let input = Complex<Float>(real: 5, imaginary: 1)
+    let expected = Complex<Float>(real: 10, imaginary: 2)
+    XCTAssertEqual(expected, input + input)
+  }
+
+  func testMinus() {
+    let inputA = Complex<Float>(real: 6, imaginary: 2)
+    let inputB = Complex<Float>(real: 5, imaginary: 1)
+    let expected = Complex<Float>(real: 1, imaginary: 1)
+    XCTAssertEqual(expected, inputA - inputB)
+  }
+
+  func testTimes() {
+    let inputA = Complex<Float>(real: 6, imaginary: 2)
+    let inputB = Complex<Float>(real: 5, imaginary: 1)
+    let expected = Complex<Float>(real: 28, imaginary: 16)
+    XCTAssertEqual(expected, inputA * inputB)
+  }
+
+  func testNegate() {
+    var input = Complex<Float>(real: 6, imaginary: 2)
+    let negated = Complex<Float>(real: -6, imaginary: -2)
+    XCTAssertEqual(-input, negated)
+    input.negate()
+    XCTAssertEqual(input, negated)
+  }
+
+  func testDivide() {
+    let inputA = Complex<Float>(real: 20, imaginary: -4)
+    let inputB = Complex<Float>(real: 3, imaginary: 2)
+    let expected = Complex<Float>(real: 4, imaginary: -4)
+    XCTAssertEqual(expected, inputA / inputB)
+  }
+
+  func testComplexConjugate() {
+    var input = Complex<Float>(real: 2, imaginary: -4)
+    var expected = Complex<Float>(real: 2, imaginary: 4)
+    XCTAssertEqual(expected, input.complexConjugate())
+
+    input = Complex<Float>(real: -2, imaginary: -4)
+    expected = Complex<Float>(real: -2, imaginary: 4)
+    XCTAssertEqual(expected, input.complexConjugate())
+
+    input = Complex<Float>(real: 2, imaginary: 4)
+    expected = Complex<Float>(real: 2, imaginary: -4)
+    XCTAssertEqual(expected, input.complexConjugate())
+  }
+
+  func testAdding() {
+    var input = Complex<Float>(real: 2, imaginary: -4)
+    var expected = Complex<Float>(real: 3, imaginary: -4)
+    XCTAssertEqual(expected, input.adding(real: 1))
+
+    input = Complex<Float>(real: 2, imaginary: -4)
+    expected = Complex<Float>(real: 2, imaginary: -3)
+    XCTAssertEqual(expected, input.adding(imaginary: 1))
+  }
+
+  func testSubtracting() {
+    var input = Complex<Float>(real: 2, imaginary: -4)
+    var expected = Complex<Float>(real: 1, imaginary: -4)
+    XCTAssertEqual(expected, input.subtracting(real: 1))
+
+    input = Complex<Float>(real: 2, imaginary: -4)
+    expected = Complex<Float>(real: 2, imaginary: -5)
+    XCTAssertEqual(expected, input.subtracting(imaginary: 1))
+  }
+
+  func testVjpAdd() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 2, imaginary: 3)) { x in
+      return x + Complex<Float>(real: 5, imaginary: 6)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: 1, imaginary: 1))
+  }
+
+  func testVjpSubtract() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 2, imaginary: 3)) { x in
+      return Complex<Float>(real: 5, imaginary: 6) - x
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: -1, imaginary: -1))
+  }
+
+  func testVjpMultiply() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 2, imaginary: 3)) { x in
+      return x * x
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 4, imaginary: 6))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: -6, imaginary: 4))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: -2, imaginary: 10))
+  }
+
+  func testVjpDivide() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x / Complex<Float>(real: 2, imaginary: 2)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 0.25, imaginary: -0.25))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0.25, imaginary: 0.25))
+  }
+
+  func testVjpNegate() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return -x
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: -1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: -1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: -1, imaginary: -1))
+  }
+
+  func testVjpComplexConjugate() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x.complexConjugate()
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: -1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: -1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: -1, imaginary: -1))
+  }
+
+  func testVjpAdding() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x.adding(real: 5)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: 1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: 1, imaginary: 1))
+  }
+
+  func testVjpAdding() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x.adding(imaginary: 5)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: 1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: 1, imaginary: 1))
+  }
+
+  func testVjpSubtracting() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x.subtracting(real: 5)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: 1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: 1, imaginary: 1))
+  }
+
+  func testVjpSubtracting() {
+    let pb: (Complex<Float>) -> Complex<Float> = pullback(at: Complex<Float>(real: 20, imaginary: -4)) { x in
+      return x.subtracting(imaginary: 5)
+    }
+    expectEqual(pb(Complex(real: 1, imaginary: 0)), Complex<Float>(real: 1, imaginary: 0))
+    expectEqual(pb(Complex(real: 0, imaginary: 1)), Complex<Float>(real: 0, imaginary: 1))
+    expectEqual(pb(Complex(real: 1, imaginary: 1)), Complex<Float>(real: 1, imaginary: 1))
+  }
 }
 
-ComplexTests.test("Static Imaginary") {
-  let imaginary = Complex<Float>(real: 0, imaginary: 1)
-  expectEqual(imaginary, Complex.i)
-}
-
-ComplexTests.test("isFinite") {
-  var complex = Complex<Float>(real: 999, imaginary: 0)
-  expectTrue(complex.isFinite)
-
-  complex = Complex(real: 1.0 / 0.0, imaginary: 1)
-  expectFalse(complex.isFinite)
-
-  complex = Complex(real: 1.0 / 0.0, imaginary: 1.0 / 0.0)
-  expectFalse(complex.isFinite)
-}
-
-ComplexTests.test("isInfinite") {
-  var complex = Complex<Float>(real: 999, imaginary: 0)
-  expectFalse(complex.isInfinite)
-
-  complex = Complex(real: 1.0 / 0.0, imaginary: 1)
-  expectTrue(complex.isInfinite)
-
-  complex = Complex(real: 1.0 / 0.0, imaginary: 1.0 / 0.0)
-  expectTrue(complex.isInfinite)
-}
-
-ComplexTests.test("isNaN") {
-  var complex = Complex<Float>(real: 999, imaginary: 0)
-  expectFalse(complex.isNaN)
-
-  complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 1)
-  expectTrue(complex.isNaN)
-
-  complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0.0 * 1.0 / 0.0)
-  expectTrue(complex.isNaN)
-}
-
-ComplexTests.test("isZero") {
-  var complex = Complex<Float>(real: 999, imaginary: 0)
-  expectFalse(complex.isZero)
-
-  complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0)
-  expectFalse(complex.isZero)
-
-  complex = Complex(real: 0.0 * 1.0 / 0.0, imaginary: 0.0 * 1.0 / 0.0)
-  expectFalse(complex.isZero)
-
-  complex = Complex(real: 0, imaginary: 0)
-  expectTrue(complex.isZero)
-}
-
-ComplexTests.test("==") {
-  var complexA = Complex<Float>(real: 999, imaginary: 0)
-  let complexB = Complex<Float>(real: 999, imaginary: 0)
-  expectEqual(complexA, complexB)
-
-  complexA = Complex(real: 5, imaginary: 0)
-  expectNotEqual(complexA, complexB)
-}
-
-ComplexTests.test("+") {
-  let input = Complex<Float>(real: 5, imaginary: 1)
-  let expected = Complex<Float>(real: 10, imaginary: 2)
-  expectEqual(expected, input + input)
-}
-
-ComplexTests.test("-") {
-  let inputA = Complex<Float>(real: 6, imaginary: 2)
-  let inputB = Complex<Float>(real: 5, imaginary: 1)
-  let expected = Complex<Float>(real: 1, imaginary: 1)
-  expectEqual(expected, inputA - inputB)
-}
-
-ComplexTests.test("*") {
-  let inputA = Complex<Float>(real: 6, imaginary: 2)
-  let inputB = Complex<Float>(real: 5, imaginary: 1)
-  let expected = Complex<Float>(real: 28, imaginary: 16)
-  expectEqual(expected, inputA * inputB)
-}
-
-ComplexTests.test("negate") {
-  var input = Complex<Float>(real: 6, imaginary: 2)
-  let negated = Complex<Float>(real: -6, imaginary: -2)
-  expectEqual(-input, negated)
-  input.negate()
-  expectEqual(input, negated)
-}
-
-ComplexTests.test("/") {
-  let inputA = Complex<Float>(real: 20, imaginary: -4)
-  let inputB = Complex<Float>(real: 3, imaginary: 2)
-  let expected = Complex<Float>(real: 4, imaginary: -4)
-  expectEqual(expected, inputA / inputB)
-}
-
-ComplexTests.test("complexConjugate") {
-  var input = Complex<Float>(real: 2, imaginary: -4)
-  var expected = Complex<Float>(real: 2, imaginary: 4)
-  expectEqual(expected, input.complexConjugate())
-
-  input = Complex<Float>(real: -2, imaginary: -4)
-  expected = Complex<Float>(real: -2, imaginary: 4)
-  expectEqual(expected, input.complexConjugate())
-
-  input = Complex<Float>(real: 2, imaginary: 4)
-  expected = Complex<Float>(real: 2, imaginary: -4)
-  expectEqual(expected, input.complexConjugate())
-}
-
-ComplexTests.test("adding") {
-  var input = Complex<Float>(real: 2, imaginary: -4)
-  var expected = Complex<Float>(real: 3, imaginary: -4)
-  expectEqual(expected, input.adding(real: 1))
-
-  input = Complex<Float>(real: 2, imaginary: -4)
-  expected = Complex<Float>(real: 2, imaginary: -3)
-  expectEqual(expected, input.adding(imaginary: 1))
-}
-
-ComplexTests.test("subtracting") {
-  var input = Complex<Float>(real: 2, imaginary: -4)
-  var expected = Complex<Float>(real: 1, imaginary: -4)
-  expectEqual(expected, input.subtracting(real: 1))
-
-  input = Complex<Float>(real: 2, imaginary: -4)
-  expected = Complex<Float>(real: 2, imaginary: -5)
-  expectEqual(expected, input.subtracting(imaginary: 1))
-}
 
 runAllTests()
