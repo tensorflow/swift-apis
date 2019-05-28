f9c3d8d68ac5e5fbfcdc0b1f1bfc811a363290c8
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-28 02:39:47 -0400

    Fixed a typo.

--- Tests/TensorFlowTests/OperatorTests/DatasetTests.swift
+++ Tests/TensorFlowTests/OperatorTests/DatasetTests.swift
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 import XCTest
-import DeepLearning
+import TensorFlow
 
-struct SimpleOutput : TensorGroup {
+struct SimpleOutput: TensorGroup {
     let a: TensorHandle<Int32>
     let b: TensorHandle<Int32>
 }
