a01f1619becb8bf94f187905d4e0928cd70a27b0
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-30 20:41:06 -0400

    Added a test for 'log1mexp'.

--- Tests/TensorFlowTests/OperatorTests/MathTests.swift
+++ Tests/TensorFlowTests/OperatorTests/MathTests.swift
@@ -16,6 +16,12 @@ import XCTest
 @testable import TensorFlow
 
 final class MathOperatorTests: XCTestCase {
+    func testLog1mexp() {
+        let x = Tensor<Float>([[-1, -2, -3, -4, -5], [-1, -2, -3, -4, -5]])
+        let y = log1mexp(x)
+        assertEqual(y, log(1 - exp(x)), accuracy: 0.0001)
+    }
+
     func testReduction() {
         // 2 x 5
         let x = Tensor<Float>([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
