9a420a16656b544442d224420dab29ebf02cc853
Dash2507 <darshankathiriya121@gmail.com>
2019-06-10 10:46:10 +0530

    Update Tests/TensorFlowTests/OperatorTests/BasicTests.swift
    Co-Authored-By: Richard Wei <rxwei@google.com>

--- Tests/TensorFlowTests/OperatorTests/BasicTests.swift
+++ Tests/TensorFlowTests/OperatorTests/BasicTests.swift
@@ -42,7 +42,7 @@ final class BasicOperatorTests: XCTestCase {
     func testVJPPadded() {
         let x = Tensor<Float>(ones: [3, 2])
         let target = Tensor<Float>([[2, 2], [2, 2], [2, 2]])
-        let grads = x.gradient{ a -> Tensor<Float> in
+        let grads = x.gradient { a -> Tensor<Float> in
             let paddedTensor = a.padded(forSizes: [(1, 0), (0, 1)], with: 3.0)
             return (paddedTensor * paddedTensor).sum()
         }
