99c7d075f83167560aa9898f502efbcfe8b4bfe8
Dash2507 <darshankathiriya121@gmail.com>
2019-06-10 10:42:34 +0530

    Update Sources/TensorFlow/Operators/Basic.swift
    Co-Authored-By: Richard Wei <rxwei@google.com>

--- Sources/TensorFlow/Operators/Basic.swift
+++ Sources/TensorFlow/Operators/Basic.swift
@@ -693,7 +693,7 @@ internal extension Tensor where Scalar: TensorFlowFloatingPoint {
         return (result, { [rank = rankTensor, shape = shapeTensor] v in
             let paddings = Tensor<Int32>(
                 shape: [sizes.count, 2],
-                    scalars: sizes.flatMap { [Int32($0.before), Int32($0.after)] })
+                scalars: sizes.flatMap { [Int32($0.before), Int32($0.after)] })
             let padBefore = Raw.slice(paddings,
                     begin: Tensor<Int32>([0, 0]),
                     size: Tensor<Int32>(stacking: [rank, Tensor<Int32>(1)]))
