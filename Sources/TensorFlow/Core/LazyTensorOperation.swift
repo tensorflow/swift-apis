41cd36442a480f1891bf8fdbe44cf76395fbaedb
Dan Zheng <danielzheng@google.com>
2019-06-12 18:37:40 -0700

    Fix warnings.

--- Sources/TensorFlow/Core/LazyTensorOperation.swift
+++ Sources/TensorFlow/Core/LazyTensorOperation.swift
@@ -33,7 +33,7 @@ class LazyTensor: _AnyTensorHandle {
         switch handle {
         case .concrete(let h, _):
             return h
-        case .symbolic(let op, let index, _):
+        case .symbolic(_, _, _):
             assert(false, "TODO: to be send out in a separate PR.")
             // return op.materialized(index: index)
         }
