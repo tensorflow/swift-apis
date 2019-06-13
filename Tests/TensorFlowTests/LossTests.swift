6b0a3c4ee221d332a0c1bfc87f5af74c9fcbc094
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2019-06-13 18:12:11 +0530

    adding tests to var all tests

--- Tests/TensorFlowTests/LossTests.swift
+++ Tests/TensorFlowTests/LossTests.swift
@@ -28,11 +28,11 @@ final class LossTests: XCTestCase {
     func testL2Loss() {
         let predicted = Tensor<Float>([1, 2, 3, 4]))
         let expected = Tensor<Float>([0.5, 1.5, 2.5, 3.5])
-        let loss = meanSquaredError(predicted: predicted, expected: expected)
+        let loss = l2Loss(predicted: predicted, expected: expected)
         let expectedLoss: Float = 1.0
         assertElementsEqual(expected: Tensor(expectedLoss), actual: loss)
     }
-    
+
     func testMeanSquaredErrorLoss() {
         let predicted = Tensor<Float>(shape: [2, 4], scalars: [1, 2, 3, 4, 5, 6, 7, 8])
         let expected = Tensor<Float>(
@@ -211,6 +211,8 @@ final class LossTests: XCTestCase {
     }
 
     static var allTests = [
+        ("testL1Loss", testL1Loss),
+        ("testL2Loss", testL2Loss),
         ("testMeanSquaredErrorLoss", testMeanSquaredErrorLoss),
         ("testMeanSquaredErrorGrad", testMeanSquaredErrorGrad),
         ("testMeanSquaredLogarithmicError", testMeanSquaredLogarithmicError),
