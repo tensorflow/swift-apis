05a6e2767b8d3aa7d2b923d2f7b76e7e787bd17c
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2019-06-14 16:50:27 +0530

    Extra bracket in one of the tests

--- Tests/TensorFlowTests/OperatorTests/MathTests.swift
+++ Tests/TensorFlowTests/OperatorTests/MathTests.swift
@@ -217,14 +217,14 @@ final class MathOperatorTests: XCTestCase {
     func testSoftplus() {
       let x = Tensor<Float>([1.0, 2.0, 3.0])
       let y = softplus(x)
-      let expected = Tensor<Float>([[1.3132616,  2.126928, 3.0485873]])
+      let expected = Tensor<Float>([1.3132616,  2.126928, 3.0485873])
       XCTAssertEqual(x, expected)
     }
 
     func testSoftsign() {
       let x = Tensor<Float>([1.0, 4.0, 3.0])
       let y = softsign(x)
-      let expected = Tensor<Float>([0.5 , 0.8 , 0.75]])
+      let expected = Tensor<Float>([0.5 , 0.8 , 0.75])
       XCTAssertEqual(x, expected)
     }
 
