9a59c89cb523d39653c17a44567597952905338e
Pawan Sasanka Ammanamanchi <Shashi456@users.noreply.github.com>
2019-06-19 10:25:01 +0530

    Add exponential linear unit (#252)

--- Sources/TensorFlow/Operators/Math.swift
+++ Sources/TensorFlow/Operators/Math.swift
@@ -996,6 +996,24 @@ func _vjpLogSoftmax<T: TensorFlowFloatingPoint>(
     return (value, { v in v - v.sum(alongAxes: -1) * exp(value) })
 }
 
+/// Returns a tensor by applying an exponential linear unit.
+/// Specifically, computes `exp(features) - 1` if < 0, `features` otherwise.
+/// See [Fast and Accurate Deep Network Learning by Exponential Linear Units (ELUs)
+/// ](http://arxiv.org/abs/1511.07289)
+@inlinable
+@differentiable(vjp: _vjpElu)
+public func elu<T: TensorFlowFloatingPoint>(_ x: Tensor<T>) -> Tensor<T> {
+    Raw.elu(features: x)
+}
+
+@inlinable
+func _vjpElu<T: TensorFlowFloatingPoint>(
+    _ x: Tensor<T>
+) -> (Tensor<T>, (Tensor<T>) -> Tensor<T>) {
+    let y = elu(x)
+    return (y, { v in Raw.eluGrad(gradients: v, outputs: y) })
+}
+
 /// Computes `relu` of the specified tensor element-wise.
 /// Specifically, computes `max(0, x)`.
 @inlinable
