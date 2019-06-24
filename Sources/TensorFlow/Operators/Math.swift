ae5e3d2ec58a907431f2c60454518a88cbcbaa6b
Anthony Platanios <e.a.platanios@gmail.com>
2019-06-23 21:58:35 -0400

    Update Sources/TensorFlow/Operators/Math.swift
    Co-Authored-By: Richard Wei <rxwei@google.com>

--- Sources/TensorFlow/Operators/Math.swift
+++ Sources/TensorFlow/Operators/Math.swift
@@ -579,7 +579,7 @@ func _vjpLog1p<T: TensorFlowFloatingPoint>(
 /// Returns `log(1 - exp(x))` using a numerically stable approach.
 ///
 /// - Note: The approach is shown in Equation 7 of:
-/// https://cran.r-project.org/web/packages/Rmpfr/vignettes/log1mexp-note.pdf.
+///   https://cran.r-project.org/web/packages/Rmpfr/vignettes/log1mexp-note.pdf.
 @inlinable
 @differentiable
 public func log1mexp<T: TensorFlowFloatingPoint>(_ x: Tensor<T>) -> Tensor<T> {
