11505817fbf55c233351ed4087be76bb3f7c0a3a
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2019-06-24 18:08:18 +0530

    Updating tconv2d test

--- Tests/TensorFlowTests/LayerTests.swift
+++ Tests/TensorFlowTests/LayerTests.swift
@@ -117,11 +117,11 @@ final class LayerTests: XCTestCase {
     }
 
     func testTransposedConv2D() {
-        let filter =  Tensor(shape: [1, 4, 2, 1], scalars: (0..<8).map(Float.init))
+        let filter =  Tensor(shape: [2, 2, 2, 1], scalars: (0..<8).map(Float.init))
         let bias = Tensor<Float>([8])
         let layer = Conv2D<Float>(filter: filter, bias: bias, activation: identity,
                                   strides: (1, 1), padding: .valid)
-        let input = Tensor(shape: [1, 4, 2, 2], scalars: (0..<8).map(Float.init))
+        let input = Tensor(shape: [1, 4, 2, 1], scalars: (0..<8).map(Float.init))
         let output = layer.inferring(from: input)
         let expected = Tensor<Float>(shape: [1, 4, 2, 1],
                                      scalars: [0, 4, 4, 20, 16, 56, 40, 104])
