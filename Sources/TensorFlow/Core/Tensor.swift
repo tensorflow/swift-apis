60f63e1ff4f844cea6ec1862c862a7d7f975e21e
Dan Zheng <danielzheng@google.com>
2019-06-26 20:28:16 -0700

    Conform `Tensor` to `PointwiseMultiplicative`. (#297)
    Friend PR: https://github.com/apple/swift/pull/25772

--- Sources/TensorFlow/Core/Tensor.swift
+++ Sources/TensorFlow/Core/Tensor.swift
@@ -500,11 +500,9 @@ extension Tensor: Codable where Scalar: Codable {
 //===------------------------------------------------------------------------------------------===//
 
 extension Tensor: AdditiveArithmetic where Scalar: Numeric {
-    /// A scalar zero tensor.
+    /// The scalar zero tensor.
     @inlinable
-    public static var zero: Tensor {
-        return Tensor(0)
-    }
+    public static var zero: Tensor { Tensor(0) }
 
     /// Adds two tensors and produces their sum.
     /// - Note: `+` supports broadcasting.
@@ -550,6 +548,26 @@ internal extension Tensor where Scalar: TensorFlowFloatingPoint {
             return (lhsGrad.sum(squeezingAxes: lhsAxes).reshaped(toShape: lhsShape),
                     rhsGrad.sum(squeezingAxes: rhsAxes).reshaped(toShape: rhsShape))
         })
+    }
+}
+
+//===------------------------------------------------------------------------------------------===//
+// Multiplicative Group
+//===------------------------------------------------------------------------------------------===//
+
+extension Tensor: PointwiseMultiplicative where Scalar: Numeric {
+    /// The scalar one tensor.
+    @inlinable
+    public static var one: Tensor { Tensor(1) }
+
+    /// Returns the pointwise reciprocal of `self`.
+    @inlinable
+    public var reciprocal: Tensor { 1 / self }
+
+    /// Multiplies two tensors element-wise and produces their product.
+    /// - Note: `.*` supports broadcasting.
+    public static func .* (lhs: Tensor, rhs: Tensor) -> Tensor {
+        return lhs * rhs
     }
 }
 
