cd537d8a6a928634ab90256b46467d26d255fa48
Anthony Platanios <e.a.platanios@gmail.com>
2019-07-01 03:07:42 -0400

    Addressed Richard's feedback.

--- Sources/TensorFlow/Random.swift
+++ Sources/TensorFlow/Random.swift
@@ -23,26 +23,26 @@ public func randomSeedForTensorFlow() -> (Int32, Int32) {
     (Int32.random(in: Int32.min..<Int32.max), Int32.random(in: Int32.min..<Int32.max))
 }
 
-//===--------------------------------------------------------------------------------------------------------------===//
+//===------------------------------------------------------------------------------------------===//
 // Random Number Generators
-//===--------------------------------------------------------------------------------------------------------------===//
+//===------------------------------------------------------------------------------------------===//
 
 /// A type-erased random number generator.
 ///
-/// The `AnyRandomNumberGenerator` type forwards random number generating operations to an underlying random number 
-/// generator, hiding its specific underlying type.
+/// The `AnyRandomNumberGenerator` type forwards random number generating operations to an 
+/// underlying random number generator, hiding its specific underlying type.
 internal struct AnyRandomNumberGenerator: RandomNumberGenerator {
     @usableFromInline
-    internal var _rng: RandomNumberGenerator
+    var _rng: RandomNumberGenerator
 
     /// - Parameter rng: A random number generator.
     @inlinable
-    public init(_ rng: RandomNumberGenerator) {
+    init(_ rng: RandomNumberGenerator) {
         self._rng = rng
     }
 
     @inlinable
-    public mutating func next() -> UInt64 {
+    mutating func next() -> UInt64 {
         return self._rng.next()
     }
 }
