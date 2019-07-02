30d231b4eaf88d7cf9fe1f7f6b35448c03dbb28b
Anthony Platanios <e.a.platanios@gmail.com>
2019-07-02 12:49:02 -0400

    Enhanced the random seed generation function.

--- Sources/TensorFlow/Core/Utilities.swift
+++ Sources/TensorFlow/Core/Utilities.swift
@@ -167,3 +167,176 @@ extension UnsafeMutablePointer where Pointee == CTensorHandle? {
         self.init(unwrapped)
     }
 }
+
+//===------------------------------------------------------------------------------------------===//
+// Hashing
+//===------------------------------------------------------------------------------------------===//
+
+internal extension FixedWidthInteger {
+    @usableFromInline
+    init(fromBytes bytes: ArraySlice<UInt8>, startingAt index: Int) {
+        if bytes.isEmpty { self = 0; return }
+        let count = bytes.count
+        self = Self(0)
+        for i in 0..<MemoryLayout<Self>.size {
+            let j = (MemoryLayout<Self>.size - i - 1) * 8
+            self |= count > 0 ? Self(bytes[index.advanced(by: i)]) << j : 0
+        }
+    }
+
+    @usableFromInline
+    func bytes(count byteCount: Int = MemoryLayout<Self>.size) -> [UInt8] {
+        let valuePointer = UnsafeMutablePointer<Self>.allocate(capacity: 1)
+        valuePointer.pointee = self.littleEndian
+        let bytesPointer = UnsafeMutablePointer<UInt8>(OpaquePointer(valuePointer))
+        var bytes = [UInt8](repeating: 0, count: byteCount)
+        for i in 0..<Swift.min(MemoryLayout<Self>.size, byteCount) {
+            bytes[byteCount - 1 - i] = (bytesPointer + i).pointee
+        }
+        valuePointer.deinitialize(count: 1)
+        valuePointer.deallocate()
+        return bytes
+    }
+
+    @usableFromInline
+    func rotate(rightBy count: Self) -> Self {
+        (self >> count) | (self << (Self(MemoryLayout<Self>.size) * 8 - count))
+    }
+}
+
+internal extension Array where Element == UInt8 {
+    func sha512() -> [UInt8] {
+        // First we define some useful constants.
+        let blockSize = 128
+        let digestLength = 64
+        let k: [UInt64] = [
+            0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+            0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+            0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+            0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+            0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+            0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+            0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+            0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+            0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+            0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+            0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+            0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+            0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+            0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+            0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+            0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+            0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+            0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+            0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+            0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817]
+
+        var accumulated = self
+        let lengthInBits = accumulated.count * 8
+        let lengthBytes = lengthInBits.bytes(count: blockSize / 8)
+        
+        // Step 1: Append padding.
+        let msgLength = accumulated.count
+        // Append one bit(`UInt8` with one bit) to the message.
+        accumulated.append(0x80)
+        // Append `0` bits until the length of `accumulated` in bits is 448 (mod 512).
+        let max = blockSize * 7 / 8
+        accumulated += [UInt8](
+            repeating: 0,
+            count: msgLength % blockSize < max ?
+                max - 1 - (msgLength % blockSize) :
+                blockSize + max - 1 - (msgLength % blockSize))
+
+        // Step 2: Append the message length as a 64-bit representation of `lengthInBits`.
+        accumulated += lengthBytes
+
+        // Step 3: Process the array bytes.
+        var accumulatedHash: [UInt64] = [
+            0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 
+            0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179]
+        var index = 0
+        while index < accumulated.count {
+            let chunk = accumulated[index..<(index + blockSize)]
+            index += blockSize
+            
+            // Break chunk into sixteen 64-bit words M[j], 0 ≤ j ≤ 15, in big-endian format.
+            // Extend the sixteen 64-bit words into eighty 64-bit words:
+            let M = UnsafeMutablePointer<UInt64>.allocate(capacity: k.count)
+            M.initialize(repeating: 0, count: k.count)
+            defer {
+                M.deinitialize(count: k.count)
+                M.deallocate()
+            }
+            for x in 0..<k.count {
+                switch x {
+                case 0...15:
+                    let start = chunk.startIndex.advanced(by: x * 8)
+                    M[x] = UInt64(fromBytes: chunk, startingAt: start)
+                    break
+                default:
+                    let s0 = M[x - 15].rotate(rightBy: 1) ^ 
+                        M[x - 15].rotate(rightBy: 8) ^ 
+                        (M[x - 15] >> 7)
+                    let s1 = M[x - 2].rotate(rightBy: 19) ^ 
+                        M[x - 2].rotate(rightBy: 61) ^ 
+                        (M[x - 2] >> 6)
+                    M[x] = M[x - 16] &+ s0 &+ M[x - 7] &+ s1
+                    break
+                }
+            }
+
+            var A = accumulatedHash[0]
+            var B = accumulatedHash[1]
+            var C = accumulatedHash[2]
+            var D = accumulatedHash[3]
+            var E = accumulatedHash[4]
+            var F = accumulatedHash[5]
+            var G = accumulatedHash[6]
+            var H = accumulatedHash[7]
+
+            for j in 0..<k.count {
+                let s0 = A.rotate(rightBy: 28) ^ A.rotate(rightBy: 34) ^ A.rotate(rightBy: 39)
+                let maj = (A & B) ^ (A & C) ^ (B & C)
+                let t2 = s0 &+ maj
+                let s1 = E.rotate(rightBy: 14) ^ E.rotate(rightBy: 18) ^ E.rotate(rightBy: 41)
+                let ch = (E & F) ^ ((~E) & G)
+                let t1 = H &+ s1 &+ ch &+ k[j] &+ UInt64(M[j])
+                H = G
+                G = F
+                F = E
+                E = D &+ t1
+                D = C
+                C = B
+                B = A
+                A = t1 &+ t2
+            }
+
+            accumulatedHash[0] = (accumulatedHash[0] &+ A)
+            accumulatedHash[1] = (accumulatedHash[1] &+ B)
+            accumulatedHash[2] = (accumulatedHash[2] &+ C)
+            accumulatedHash[3] = (accumulatedHash[3] &+ D)
+            accumulatedHash[4] = (accumulatedHash[4] &+ E)
+            accumulatedHash[5] = (accumulatedHash[5] &+ F)
+            accumulatedHash[6] = (accumulatedHash[6] &+ G)
+            accumulatedHash[7] = (accumulatedHash[7] &+ H)
+        }
+
+        // Step 4: Return the computed hash.
+        var result = [UInt8](repeating: 0, count: digestLength)
+        var position = 0
+        for index in 0..<accumulatedHash.count {
+            let h = accumulatedHash[index]
+            result[position + 0] = UInt8((h >> 56) & 0xff)
+            result[position + 1] = UInt8((h >> 48) & 0xff)
+            result[position + 2] = UInt8((h >> 40) & 0xff)
+            result[position + 3] = UInt8((h >> 32) & 0xff)
+            result[position + 4] = UInt8((h >> 24) & 0xff)
+            result[position + 5] = UInt8((h >> 16) & 0xff)
+            result[position + 6] = UInt8((h >> 8) & 0xff)
+            result[position + 7] = UInt8(h & 0xff)
+            position += 8
+        }
+
+        return result
+    }
+}
