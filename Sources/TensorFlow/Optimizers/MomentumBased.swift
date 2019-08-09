68318fa8081e68a89084474c0c4f2ff1762c04c4
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2019-08-09 12:19:35 +0530

    Adding LAMB optimizer

--- Sources/TensorFlow/Optimizers/MomentumBased.swift
+++ Sources/TensorFlow/Optimizers/MomentumBased.swift
@@ -14,8 +14,8 @@
 
 /// RMSProp optimizer.
 ///
-/// It is recommended to leave the parameters of this optimizer at their default values (except for 
-/// the learning rate, which can be freely tuned). This optimizer is usually a good choice for 
+/// It is recommended to leave the parameters of this optimizer at their default values (except for
+/// the learning rate, which can be freely tuned). This optimizer is usually a good choice for
 /// recurrent neural networks.
 ///
 /// Reference: ["rmsprop: Divide the gradient by a running average of its recent magnitude"](
@@ -65,7 +65,7 @@ public class RMSProp<Model: Differentiable>: Optimizer
 
 /// AdaGrad optimizer.
 ///
-/// Individually adapts the learning rates of all model parameters by scaling them inversely 
+/// Individually adapts the learning rates of all model parameters by scaling them inversely
 /// proportional to the square root of the sum of all the historical squared values of the gradient.
 ///
 /// Reference: ["Adaptive Subgradient Methods for Online Learning and Stochastic Optimization"](
@@ -108,9 +108,9 @@ public class AdaGrad<Model: Differentiable>: Optimizer
 /// ADADELTA optimizer.
 ///
 /// ADADELTA is a more robust extension of AdaGrad. ADADELTA adapts learning rates based on a moving
-/// window of gradient updates rather than by accumulating all past gradient norms. It can thus 
+/// window of gradient updates rather than by accumulating all past gradient norms. It can thus
 /// adapt faster to changing dynamics of the optimization problem space.
-/// 
+///
 /// Reference: ["ADADELTA: An Adaptive Learning Rate Method"](https://arxiv.org/abs/1212.5701)
 public class AdaDelta<Model: Differentiable>: Optimizer
     where Model.TangentVector: VectorProtocol & PointwiseMultiplicative & ElementaryFunctions,
@@ -209,7 +209,7 @@ public class Adam<Model: Differentiable>: Optimizer
         step += 1
         let step = Float(self.step)
         let learningRate = self.learningRate * 1 / (1 + decay * step)
-        // Note: `stepSize` and `secondMoments` are split into two lines to avoid the "compiler is 
+        // Note: `stepSize` and `secondMoments` are split into two lines to avoid the "compiler is
         // unable to type-check this expression in reasonable time" error.
         var stepSize = learningRate * sqrt(1 - pow(beta2, step))
         stepSize = stepSize / (1 - pow(beta1, step))
@@ -228,7 +228,7 @@ public class Adam<Model: Differentiable>: Optimizer
 /// Reference: Section 7 of ["Adam - A Method for Stochastic Optimization"](
 /// https://arxiv.org/abs/1412.6980v8)
 public class AdaMax<Model: Differentiable & KeyPathIterable>: Optimizer
-    where Model.TangentVector: VectorProtocol & PointwiseMultiplicative & 
+    where Model.TangentVector: VectorProtocol & PointwiseMultiplicative &
                                ElementaryFunctions & KeyPathIterable,
           Model.TangentVector.VectorSpaceScalar == Float {
     public typealias Model = Model
@@ -280,7 +280,7 @@ public class AdaMax<Model: Differentiable & KeyPathIterable>: Optimizer
         stepSize = stepSize / (1 - pow(beta1, step))
         firstMoments = firstMoments * beta1 + direction * (1 - beta1)
 
-        // Update `infinityNorm` using a key path approach because `max(_:_:)` cannot be 
+        // Update `infinityNorm` using a key path approach because `max(_:_:)` cannot be
         // currently applied in a simpler manner.
         for kp in infinityNorm.recursivelyAllWritableKeyPaths(to: Tensor<Float>.self) {
             infinityNorm[keyPath: kp] = max(
@@ -304,7 +304,7 @@ public class AdaMax<Model: Differentiable & KeyPathIterable>: Optimizer
 /// Reference: ["On the Convergence of Adam and Beyond"](
 /// https://openreview.net/pdf?id=ryQu7f-RZ)
 public class AMSGrad<Model: Differentiable & KeyPathIterable>: Optimizer
-    where Model.TangentVector: VectorProtocol & PointwiseMultiplicative & 
+    where Model.TangentVector: VectorProtocol & PointwiseMultiplicative &
                                ElementaryFunctions & KeyPathIterable,
           Model.TangentVector.VectorSpaceScalar == Float {
     public typealias Model = Model
@@ -353,7 +353,7 @@ public class AMSGrad<Model: Differentiable & KeyPathIterable>: Optimizer
         let beta1Power = pow(beta1, step)
         let beta2Power = pow(beta2, step)
         let learningRate = self.learningRate * 1 / (1 + decay * step)
-        // Note: `stepSize` and `secondMoments` are split into two lines to avoid the "compiler is 
+        // Note: `stepSize` and `secondMoments` are split into two lines to avoid the "compiler is
         // unable to type-check this expression in reasonable time" error.
         var stepSize = learningRate * sqrt(1 - pow(beta2Power, step))
         stepSize = stepSize / (1 - pow(beta1Power, step))
@@ -361,7 +361,7 @@ public class AMSGrad<Model: Differentiable & KeyPathIterable>: Optimizer
         secondMoments = secondMoments * beta2
         secondMoments += direction .* direction * (1 - beta2)
 
-        // Update `secondMomentsMax` using a key path approach because `max(_:_:)` cannot be 
+        // Update `secondMomentsMax` using a key path approach because `max(_:_:)` cannot be
         // currently applied in a simpler manner.
         for kp in secondMomentsMax.recursivelyAllWritableKeyPaths(to: Tensor<Float>.self) {
             secondMomentsMax[keyPath: kp] = max(
@@ -374,5 +374,79 @@ public class AMSGrad<Model: Differentiable & KeyPathIterable>: Optimizer
 
         let denominator = Model.TangentVector.sqrt(secondMomentsMax) + epsilon
         model.move(along: -stepSize * firstMoments ./ denominator)
+    }
+}
+
+/// LAMB optimizer.
+///
+/// Reference: ["Large Batch Optimization for Deep Learning"](
+/// https://arxiv.org/abs/1904.00962)
+public class LAMB<Model: Differentiable>: Optimizer
+    where Model.TangentVector: VectorProtocol & PointwiseMultiplicative & ElementaryFunctions,
+          Model.TangentVector.VectorSpaceScalar == Float {
+    public typealias Model = Model
+    /// The learning rate.
+    public var learningRate: Float
+    /// A coefficient used to calculate the first and second moments of the gradients.
+    public var beta1: Float
+    /// A coefficient used to calculate the first and second moments of the gradients.
+    public var beta2: Float
+    /// A small scalar added to the denominator to improve numerical stability.
+    public var epsilon: Float
+    /// The learning rate decay.
+    public var decay: Float
+    /// The weight decay.
+    public var weight_decay: Float
+    /// The current step.
+    public var step: Int = 0
+    /// The first moments of the weights.
+    public var firstMoments: Model.TangentVector = .zero
+    /// The second moments of the weights.
+    public var secondMoments: Model.TangentVector = .zero
+
+    public init(
+        for model: __shared Model,
+        learningRate: Float = 1e-3,
+        beta1: Float = 0.9,
+        beta2: Float = 0.999,
+        epsilon: Float = 1e-,
+        decay: Float = 0,
+        weight_decay = 0.01
+    ) {
+        precondition(learningRate >= 0, "Learning rate must be non-negative")
+        precondition(0 <= beta1 && beta1 <= 1, "Beta parameter must be between 0 and 1")
+        precondition(0 <= beta2 && beta2 <= 1, "Beta parameter must be between 0 and 1")
+        precondition(decay >= 0, "Learning rate decay must be non-negative")
+
+        self.learningRate = learningRate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.decay = decay
+    }
+
+    public func update(_ model: inout Model, along direction: Model.TangentVector) {
+        step += 1
+        let step = Float(self.step)
+        let learningRate = self.learningRate * 1 / (1 + decay * step)
+        // Note: `stepSize` and `secondMoments` are split into two lines to avoid the "compiler is
+        // unable to type-check this expression in reasonable time" error.
+        var stepSize = learningRate * sqrt(1 - pow(beta2, step))
+        stepSize = stepSize / (1 - pow(beta1, step))
+        firstMoments = firstMoments * beta1 + direction * (1 - beta1)
+        secondMoments = secondMoments * beta2
+        secondMoments += direction .* direction * (1 - beta2)
+        r1 = sqrt(pow(direction, 2).sum())
+        let denominator = Model.TangentVector.sqrt(secondMoments) + epsilon
+        adamStep = stepSize * firstMoments ./ denominator
+        r2 = sqrt(pow(adamStep, 2).sum())
+        if r1 == 0 or r2 == 0:
+            q = 1
+        else
+            q = min(r1/r2, 10)
+        learningRate = learningRate * q
+
+        model.move(along: -learningRate * adamStep)
+
     }
 }
