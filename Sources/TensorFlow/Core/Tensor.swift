f467664c257b348b215fc21ad30022d94328ab67
Richard Wei <rxwei@google.com>
2019-09-19 16:12:09 -0400

    [Tensor] Add a VJP for 'init(_:)' (scalars) and 'init(shape:scalars:)'.
    Add a VJP for the `init(_:)` which takes an array of scalars and `init(shape:scalars:)` which takes a shape and an array of scalars. Both VJPs are merely returning the original value and a transpose, because `init(_:)` and `init(shape:scalars:)` are linear.
    
    Resolves #510 and #511.

--- Sources/TensorFlow/Core/Tensor.swift
+++ Sources/TensorFlow/Core/Tensor.swift
@@ -169,7 +169,7 @@ public extension Tensor {
 }
 
 extension Tensor where Scalar: TensorFlowFloatingPoint {
-    @usableFromInline
+    @inlinable
     func _vjpScalars() -> (value: [Scalar], pullback: (Array<Scalar>.TangentVector) -> Tensor) {
         (value: scalars, pullback: { [shape = self.shape] v in
             Tensor(shape: shape, scalars: v.base)
@@ -200,6 +200,7 @@ internal extension Tensor where Scalar: TensorFlowFloatingPoint {
 public extension Tensor {
     /// Creates a 1D tensor from scalars.
     @inlinable
+    @differentiable(vjp: _vjpInit(_:) where Scalar: TensorFlowFloatingPoint)
     init(_ scalars: [Scalar]) {
         self.init(shape: [scalars.count], scalars: scalars)
     }
@@ -226,6 +227,7 @@ public extension Tensor {
     ///   - scalars: The scalar contents of the tensor.
     /// - Precondition: The product of the dimensions of the shape must equal the number of scalars.
     @inlinable
+    @differentiable(vjp: _vjpInit(shape:scalars:) where Scalar: TensorFlowFloatingPoint)
     init(shape: TensorShape, scalars: [Scalar]) {
         precondition(shape.contiguousSize == scalars.count,
             """
@@ -281,6 +283,22 @@ public extension Tensor {
                 }
             })
         self.init(handle: handle)
+    }
+}
+
+extension Tensor where Scalar: TensorFlowFloatingPoint {
+    @inlinable
+    static func _vjpInit(_ scalars: [Scalar]) -> (
+        value: Tensor, pullback: (Tensor) -> Array<Scalar>.TangentVector
+    ) {
+        (value: Tensor(scalars), pullback: { v in Array<Scalar>.TangentVector(v.scalars) })
+    }
+
+    @inlinable
+    static func _vjpInit(shape: TensorShape, scalars: [Scalar]) -> (
+        value: Tensor, pullback: (Tensor) -> Array<Scalar>.TangentVector
+    ) {
+        (value: Tensor(scalars), pullback: { v in Array<Scalar>.TangentVector(v.scalars) })
     }
 }
 
