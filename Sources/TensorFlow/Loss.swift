7dbb9b99a886363565b7326e1f74900307a32697
joao guilherme <joaoguilhermearujo@gmail.com>
2019-12-13 10:18:37 +0900

    Fix typo
    Co-Authored-By: Brad Larson <bradlarson@google.com>

--- Sources/TensorFlow/Loss.swift
+++ Sources/TensorFlow/Loss.swift
@@ -31,7 +31,7 @@ public func l1Loss<Scalar: TensorFlowFloatingPoint>(
 }
 
 /// Returns the L2 loss between predictions and expectations.
-/// Given the x and y vectors L1 loss computes:
+/// Given the x and y vectors, the L2 loss is computed as follows:
 ///  `reduction([l_1, l_2, ..., l_N]^T)`, where `l_i = (x_i - y_i)^2`
 ///   and `N` is yhe batch size
 ///
