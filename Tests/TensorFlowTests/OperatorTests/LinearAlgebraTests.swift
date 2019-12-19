a46672a6c60360dbe0e83ea81fb51165f82cba75
Dan Zheng <danielzheng@google.com>
2019-12-19 09:30:30 -0800

    Minor test fix.

--- Tests/TensorFlowTests/OperatorTests/LinearAlgebraTests.swift
+++ Tests/TensorFlowTests/OperatorTests/LinearAlgebraTests.swift
@@ -60,7 +60,7 @@ final class LinearAlgebraTests: XCTestCase {
         }
     }
     func testTrace() {
-        assertEqual(trace(Tensor<Float>(ones: [3, 3])) == Tensor(3.0))
+        assertEqual(trace(Tensor<Float>(ones: [3, 3])), Tensor(3.0), accuracy: 1e-16)
         let shapes = [[1, 3, 3], [2, 4, 4], [2, 3, 5, 5]]
         for shape in shapes {
             let x = Tensor<Float>(ones: TensorShape(shape))
