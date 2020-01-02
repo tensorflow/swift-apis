ede3c37f90c304958944ece6e55e7e390e034b6d
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2020-01-02 23:42:03 +0530

    Update license headers to 2020

--- Sources/TensorFlow/Bindings/generate_wrappers.py
+++ Sources/TensorFlow/Bindings/generate_wrappers.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ _WARNING = """// !!! THIS CODE IS AUTOMATICALLY GENERATED, DO NOT EDIT BY HAND !
 //
 """
 
-_HEADER = """// Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+_HEADER = """// Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
