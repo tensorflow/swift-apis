b26112a4353ce6e5ab93cdcb178765601864dee1
8bitmp3 <19637339+8bitmp3@users.noreply.github.com>
2020-04-15 20:13:47 +0000

    Update the AdaGrad optimizer class API docs 📒 (#675)
    - Describe the AdaGrad algorithm in greater detail.
    - Fix documentation for `initialAccumulatorValue` and `epsilon`.
    - Add `RMSProp.init` documentation.
    
    Co-authored-by: Dan Zheng <danielzheng@google.com>

--- Sources/TensorFlow/Optimizers/MomentumBased.swift
+++ Sources/TensorFlow/Optimizers/MomentumBased.swift
@@ -88,13 +88,18 @@ where
   }
 }
 
-/// AdaGrad optimizer.
+/// An AdaGrad optimizer.
 ///
-/// Individually adapts the learning rates of all model parameters by scaling them inversely 
-/// proportional to the square root of the sum of all the historical squared values of the gradient.
+/// Implements the AdaGrad (adaptive gradient) optimization algorithm. AdaGrad has
+/// parameter-specific learning rates, which are adapted relative to how frequently parameters
+/// gets updated during training. Parameters that receive more updates have smaller learning rates.
 ///
-/// Reference: ["Adaptive Subgradient Methods for Online Learning and Stochastic Optimization"](
-/// http://www.jmlr.org/papers/volume12/duchi11a/duchi11a.pdf)
+/// AdaGrad individually adapts the learning rates of all model parameters by scaling them inversely
+/// proportional to the square root of the running sum of squares of gradient norms.
+///
+/// Reference: ["Adaptive Subgradient Methods for Online Learning and Stochastic 
+/// Optimization"](http://www.jmlr.org/papers/volume12/duchi11a/duchi11a.pdf) 
+/// (Duchi et al, 2011)
 public class AdaGrad<Model: Differentiable>: Optimizer
 where
   Model.TangentVector: VectorProtocol & PointwiseMultiplicative
@@ -104,15 +109,22 @@ where
   public typealias Model = Model
   /// The learning rate.
   public var learningRate: Float
-  /// The smoothing factor (ρ). Typical values are `0.5`, `0.9`, and `0.99`, for smoothing over 2,
-  /// 10, and 100 examples, respectively.
+  /// A small scalar added to the denominator to improve numerical stability.
   public var epsilon: Float
-  /// The accumulator for square of gradient.
+  /// The running sum of squares of gradient norms.
   public var accumulator: Model.TangentVector
 
+  /// Creates an instance for `model`.
+  ///
+  /// - Parameters:
+  ///   - learningRate: The learning rate. The default value is `1e-3`.
+  ///   - initialAccumulatorValue: The starting value for the running sum of squares of gradient
+  ///     norms. The default value is `0.1`.
+  ///   - epsilon: A small scalar added to the denominator to improve numerical stability. The
+  ///     default value is `1e-8`.
   public init(
     for model: __shared Model,
-    learningRate: Float = 0.001,
+    learningRate: Float = 1e-3,
     initialAccumulatorValue: Float = 0.1,
     epsilon: Float = 1e-8
   ) {
