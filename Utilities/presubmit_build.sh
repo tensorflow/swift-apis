faa150a519aa6aa11b97b0e0ec5c8f2492dd38cd
Saleem Abdulrasool <abdulras@google.com>
2020-04-16 15:28:27 -0700

    CI: move Docker into tmpfs for space
    Try to increase the space available to docker by using tmpfs.

--- Utilities/presubmit_build.sh
+++ Utilities/presubmit_build.sh
@@ -12,6 +12,18 @@ if [[ ! -z ${swift_tf_bigstore_gfile+x} ]]; then
   export swift_tf_url="${swift_tf_bigstore_gfile/\/bigstore/https://storage.googleapis.com}"
 fi
 
+# Help debug the job's disk space.
+df -h
+
+# Move docker images into /tmpfs, where there is more space.
+sudo /etc/init.d/docker stop
+sudo mv /var/lib/docker /tmpfs/
+sudo ln -s /tmpfs/docker /var/lib/docker
+sudo /etc/init.d/docker start
+
+# Help debug the job's disk space.
+df -h
+
 cd github/swift-apis
 cp -R /opt/google-cloud-sdk .
 sudo -E docker build -t build-img -f Dockerfile --build-arg swift_tf_url .
