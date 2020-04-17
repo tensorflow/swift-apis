c8646e43cf468e6f5d3155210f3fdbc5c6a20212
Saleem Abdulrasool <compnerd@compnerd.org>
2020-04-17 10:21:09 -0700

    build: add `FindX10` (#829)
    Add the ability to use an external build of X10.  This attempts to
    search the swift toolchain in addition to the standard search order.
    The user can specify `-D X10_LIBRARY=` with a full path to the library
    and `-D X10_INCLUDE_DIRS=` for the header search path.  This should
    allow eliding the x10 build.  The local build can be forced by setting
    `USE_BUNDLED_X10` to `NO`.

---
+++ cmake/modules/FindX10.cmake
@@ -0,0 +1,27 @@
+
+if(DEFINED CMAKE_Swift_COMPILER)
+  get_filename_component(_Swift_TOOLCHAIN ${CMAKE_Swift_COMPILER} DIRECTORY)
+  get_filename_component(_Swift_TOOLCHAIN ${_Swift_TOOLCHAIN} DIRECTORY)
+  file(TO_CMAKE_PATH ${_Swift_TOOLCHAIN}/usr/lib/swift _Swift_LIBDIR)
+endif()
+
+find_library(X10_LIBRARY
+  NAMES x10
+  HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${_Swift_LIBDIR})
+
+unset(_Swift_TOOLCHAIN)
+unset(_Swift_LIBDIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(X10
+  FOUND_VAR X10_FOUND
+  REQUIRED_VARS X10_LIBRARY)
+
+if(X10_FOUND AND NOT TARGET x10)
+  add_library(x10 UNKNOWN IMPORTED)
+  set_target_properties(x10 PROPERTIES
+    IMPORTED_LOCATION ${X10_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES "${X10_INCLUDE_DIRS}")
+endif()
+
+mark_as_advanced(X10_INCLUDE_DIRS X10_LIBRARY)
