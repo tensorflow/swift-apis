e440c59e85a648f12ed8e5095a614f9fd6529b3a
Parker Schuh <parkers@google.com>
2020-04-20 12:11:33 -0700

    Remove x10_device and make it implementationOnly

--- Sources/x10/swift_bindings/apis/DeviceScope.swift
+++ Sources/x10/swift_bindings/apis/DeviceScope.swift
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@_exported import x10_device
-
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
   import Darwin
 #else
