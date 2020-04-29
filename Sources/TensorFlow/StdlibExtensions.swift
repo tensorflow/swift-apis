a6bd9f60524f52dacce7cfc70bc27454204f4076
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-04-28 18:08:40 -0700

    Move stdlib addition

--- Sources/TensorFlow/StdlibExtensions.swift
+++ Sources/TensorFlow/StdlibExtensions.swift
@@ -277,3 +277,8 @@ where Element: Differentiable & PointwiseMultiplicative {
     }
   }
 }
+
+extension Collection {
+  /// Returns the `n`th position in `self`.
+  func index(atOffset n: Int) -> Index { index(startIndex, offsetBy: n) }
+}
