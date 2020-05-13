84723b8fb0dcf508d687fcd8989f76edceaa74e7
Dave Abrahams <dabrahams@google.com>
2020-05-13 10:11:00 -0700

    Strip irrelevant stuff.

--- Sources/TensorFlow/StdlibExtensions.swift
+++ Sources/TensorFlow/StdlibExtensions.swift
@@ -12,147 +12,74 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extension Zip2Sequence: Collection 
-  where Sequence1: Collection, Sequence2: Collection 
-{
-  private var base: (Sequence1, Sequence2) {
-    return unsafeBitCast(self, to: (Sequence1, Sequence2).self)
-  }
+// MARK: - Array extensions
 
-  public struct Index: Comparable {
-    init(_ b0: Sequence1.Index, _ b1: Sequence2.Index) {
-      base = (b0, b1)
-    }
-    var base: (Sequence1.Index, Sequence2.Index)
-
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-      return lhs.base.0 == rhs.base.0
-    }
-    public static func < (lhs: Self, rhs: Self) -> Bool {
-      return lhs.base.0 < rhs.base.0
-    }
-  }
-
-  public subscript(i: Index) -> Element {
-    (base.0[i.base.0], base.1[i.base.1])
-  }
-
-  public var startIndex: Index { Index(base.0.startIndex, base.1.startIndex) }
-  public var endIndex: Index { Index(base.0.endIndex, base.1.endIndex) }
-
-  public func index(after i: Index) -> Index {
-    let r0 = base.0.index(after: i.base.0)
-    if r0 != base.0.endIndex {
-      let r1 = base.1.index(after: i.base.1)
-      if r1 != base.1.endIndex { return Index(r0, r1) }
-    }
-    return endIndex
-  }
-}
-
-extension Zip2Sequence: RandomAccessCollection, BidirectionalCollection
-  where Sequence1: RandomAccessCollection, Sequence2: RandomAccessCollection 
-{
-  public var count: Int { Swift.min(base.0.count, base.1.count) }
-  
-  func offset(of i: Index) -> Int {
-    Swift.min(base.0.offset(of: i.base.0), base.1.offset(of: i.base.1))
-  }
-
-  public func index(before i: Index) -> Index {
-    let n0 = base.0.offset(of: i.base.0)
-    let n1 = base.1.offset(of: i.base.1)
-    if n0 == n1 {
-      return Index(
-        base.0.index(before: i.base.0), base.1.index(before: i.base.1))
-    }
-    let n = Swift.min(n0, n1) - 1
-    return Index(base.0.index(atOffset: n), base.1.index(atOffset: n))
-  }
-
-  public func distance(from i: Index, to j: Index) -> Int {
-    offset(of: j) - offset(of: i)
-  }
-
-  public func index(_ i: Index, offsetBy d: Int) -> Index {
-    let n = offset(of: i) + d
-    if n != base.0.count && n != base.1.count {
-      return Index(base.0.index(atOffset: n), base.1.index(atOffset: n))
-    }
-    return endIndex
-  }
-}
-
-// MARK: - Collection extensions
-
-extension RangeReplaceableCollection 
-  where Self : ElementaryFunctions, Element: ElementaryFunctions 
-{
+extension Array: ElementaryFunctions where Element: ElementaryFunctions {
   /// The square root of `x`.
   ///
   /// For real types, if `x` is negative the result is `.nan`. For complex
   /// types there is a branch cut on the negative real axis.
-  public static func sqrt(_ x: Self) -> Self { Self(x.lazy.map(Element.sqrt)) }
+  public static func sqrt(_ x: Self) -> Self { x.map(Element.sqrt) }
 
   /// The cosine of `x`, interpreted as an angle in radians.
-  public static func cos(_ x: Self) -> Self { Self(x.lazy.map(Element.cos)) }
+  public static func cos(_ x: Self) -> Self { x.map(Element.cos) }
 
   /// The sine of `x`, interpreted as an angle in radians.
-  public static func sin(_ x: Self) -> Self { Self(x.lazy.map(Element.sin)) }
+  public static func sin(_ x: Self) -> Self { x.map(Element.sin) }
 
   /// The tangent of `x`, interpreted as an angle in radians.
-  public static func tan(_ x: Self) -> Self { Self(x.lazy.map(Element.tan)) }
+  public static func tan(_ x: Self) -> Self { x.map(Element.tan) }
 
   /// The inverse cosine of `x` in radians.
-  public static func acos(_ x: Self) -> Self { Self(x.lazy.map(Element.acos)) }
+  public static func acos(_ x: Self) -> Self { x.map(Element.acos) }
 
   /// The inverse sine of `x` in radians.
-  public static func asin(_ x: Self) -> Self { Self(x.lazy.map(Element.asin)) }
+  public static func asin(_ x: Self) -> Self { x.map(Element.asin) }
 
   /// The inverse tangent of `x` in radians.
-  public static func atan(_ x: Self) -> Self { Self(x.lazy.map(Element.atan)) }
+  public static func atan(_ x: Self) -> Self { x.map(Element.atan) }
 
   /// The hyperbolic cosine of `x`.
-  public static func cosh(_ x: Self) -> Self { Self(x.lazy.map(Element.cosh)) }
+  public static func cosh(_ x: Self) -> Self { x.map(Element.cosh) }
 
   /// The hyperbolic sine of `x`.
-  public static func sinh(_ x: Self) -> Self { Self(x.lazy.map(Element.sinh)) }
+  public static func sinh(_ x: Self) -> Self { x.map(Element.sinh) }
 
   /// The hyperbolic tangent of `x`.
-  public static func tanh(_ x: Self) -> Self { Self(x.lazy.map(Element.tanh)) }
+  public static func tanh(_ x: Self) -> Self { x.map(Element.tanh) }
 
   /// The inverse hyperbolic cosine of `x`.
-  public static func acosh(_ x: Self) -> Self { Self(x.lazy.map(Element.acosh)) }
+  public static func acosh(_ x: Self) -> Self { x.map(Element.acosh) }
 
   /// The inverse hyperbolic sine of `x`.
-  public static func asinh(_ x: Self) -> Self { Self(x.lazy.map(Element.asinh)) }
+  public static func asinh(_ x: Self) -> Self { x.map(Element.asinh) }
 
   /// The inverse hyperbolic tangent of `x`.
-  public static func atanh(_ x: Self) -> Self { Self(x.lazy.map(Element.atanh)) }
+  public static func atanh(_ x: Self) -> Self { x.map(Element.atanh) }
 
   /// The exponential function applied to `x`, or `e**x`.
-  public static func exp(_ x: Self) -> Self { Self(x.lazy.map(Element.exp)) }
+  public static func exp(_ x: Self) -> Self { x.map(Element.exp) }
 
   /// Two raised to to power `x`.
-  public static func exp2(_ x: Self) -> Self { Self(x.lazy.map(Element.exp2)) }
+  public static func exp2(_ x: Self) -> Self { x.map(Element.exp2) }
 
   /// Ten raised to to power `x`.
-  public static func exp10(_ x: Self) -> Self { Self(x.lazy.map(Element.exp10)) }
+  public static func exp10(_ x: Self) -> Self { x.map(Element.exp10) }
 
   /// `exp(x) - 1` evaluated so as to preserve accuracy close to zero.
-  public static func expm1(_ x: Self) -> Self { Self(x.lazy.map(Element.expm1)) }
+  public static func expm1(_ x: Self) -> Self { x.map(Element.expm1) }
 
   /// The natural logarithm of `x`.
-  public static func log(_ x: Self) -> Self { Self(x.lazy.map(Element.log)) }
+  public static func log(_ x: Self) -> Self { x.map(Element.log) }
 
   /// The base-two logarithm of `x`.
-  public static func log2(_ x: Self) -> Self { Self(x.lazy.map(Element.log2)) }
+  public static func log2(_ x: Self) -> Self { x.map(Element.log2) }
 
   /// The base-ten logarithm of `x`.
-  public static func log10(_ x: Self) -> Self { Self(x.lazy.map(Element.log10)) }
+  public static func log10(_ x: Self) -> Self { x.map(Element.log10) }
 
   /// `log(1 + x)` evaluated so as to preserve accuracy close to zero.
-  public static func log1p(_ x: Self) -> Self { Self(x.lazy.map(Element.log1p)) }
+  public static func log1p(_ x: Self) -> Self { x.map(Element.log1p) }
 
   /// `exp(y log(x))` computed without loss of intermediate precision.
   ///
@@ -161,32 +88,20 @@ extension RangeReplaceableCollection
   /// negative real axis.
   public static func pow(_ x: Self, _ y: Self) -> Self {
     precondition(x.count == y.count)
-    return Self(zip(x, y).lazy.map(Element.pow))
+    return zip(x, y).map(Element.pow)
   }
 
   /// `x` raised to the `n`th power.
   ///
   /// The product of `n` copies of `x`.
-  public static func pow(_ x: Self, _ n: Int) -> Self { 
-    Self(x.lazy.map { Element.pow($0, n) }) 
-  }
+  public static func pow(_ x: Self, _ n: Int) -> Self { x.map { Element.pow($0, n) } }
 
   /// The `n`th root of `x`.
   ///
   /// For real types, if `x` is negative and `n` is even, the result is NaN.
   /// For complex types, there is a branch cut along the negative real axis.
-  public static func root(_ x: Self, _ n: Int) -> Self { 
-    Self(x.lazy.map { Element.root($0, n) })
-  }
+  public static func root(_ x: Self, _ n: Int) -> Self { x.map { Element.root($0, n) } }
 }
-
-extension Array : ElementaryFunctions where Element: ElementaryFunctions {  }
-extension ArraySlice : ElementaryFunctions 
-  where Element: ElementaryFunctions {  }
-extension ContiguousArray : ElementaryFunctions 
-  where Element: ElementaryFunctions {  }
-extension Slice : ElementaryFunctions 
-  where Base : RangeReplaceableCollection, Element: ElementaryFunctions {  }
 
 // MARK: - Array derivative extensions
 
@@ -366,7 +281,4 @@ where Element: Differentiable & PointwiseMultiplicative {
 extension Collection {
   /// Returns the `n`th position in `self`.
   func index(atOffset n: Int) -> Index { index(startIndex, offsetBy: n) }
-  
-  /// Returns the offset of `i` in `self` from `self.startIndex`.
-  func offset(of i: Index) -> Int { distance(from: startIndex, to: i) }
 }
