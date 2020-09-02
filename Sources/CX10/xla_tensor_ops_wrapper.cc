adaa050ec12917d7313bed82a79642d8200e9aaa
Parker Schuh <parkers@google.com>
2020-09-02 14:00:47 -0700

    Generate log_softmax_backward from a spec. (#1070)

---
+++ Sources/CX10/xla_tensor_ops_wrapper.cc
@@ -0,0 +1,30 @@
+// Copyright 2020 TensorFlow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if defined(_WIN32)
+#define XLA_API __declspec(dllexport)
+#else
+#define XLA_API __attribute__((__visibility__("default")))
+#endif
+
+#include "xla_tensor_wrapper.h"
+
+#include "tensorflow/compiler/xla/xla_client/debug_macros.h"
+#include "tensorflow/compiler/xla/xla_client/util.h"
+#include "tensorflow/compiler/tf2xla/xla_tensor/helpers.h"
+#include "tensorflow/compiler/tf2xla/xla_tensor/lowering_context.h"
+#include "tensorflow/compiler/tf2xla/xla_tensor/ops/infer_output_shape.h"
+#include "tensorflow/compiler/tf2xla/xla_tensor/softmax_builder.h"
+
+#include "xla_tensor_ops_wrapper_generated.cc.inc"
