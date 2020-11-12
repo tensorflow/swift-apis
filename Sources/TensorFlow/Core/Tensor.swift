7a1c9310e92ce7e4ea7f70b527537d75b1533c55
Saleem Abdulrasool <compnerd@compnerd.org>
2020-11-12 08:47:38 -0800

    TensorFlow: import `_Differentiation` more thoroughly (#1121)
    While experimenting with building with the standard toolchain, these
    sites were uncovered as requiring additional importing of the
    `_Differentiation` module.  When building with the tensorflow branch,
    this import is implicit.  This should be an entirely invisible change to
    users.

--- Sources/TensorFlow/Core/Tensor.swift
+++ Sources/TensorFlow/Core/Tensor.swift
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import CTensorFlow
 import _Differentiation
+import CTensorFlow
 
 infix operator .==: ComparisonPrecedence
 infix operator .!=: ComparisonPrecedence
