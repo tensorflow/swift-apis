2f423d6c45e81b4e733b7a7760fe1bfe144e6af9
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-15 14:04:40 -0800

    build: use the build to fetch and cache X10
    This enables the X10 build with SPM.  Use of stock toolchains simply
    requires an additional `-Xswiftc -DTENSORFLOW_USE_STANDARD_TOOLCHAIN`.
    This prototype demonstrates a complete build of Swift APIs using SPM as
    a plausible replacement for CMake.

--- Package.swift
+++ Package.swift
@@ -17,6 +17,85 @@
 
 import PackageDescription
 
+import Dispatch
+import Foundation
+#if !os(macOS)
+import FoundationNetworking
+#endif
+
+func extract(_ url: URL, _ cache: URL) throws {
+  let process: Process = Process()
+#if os(Windows)
+  process.executableURL =
+      URL(fileURLWithPath: "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe")
+
+  let path = url.withUnsafeFileSystemRepresentation {
+    String(cString: $0!)
+  }
+  process.arguments = [
+    "-Command",
+    "Expand-Archive -Force \"\(path)\" \"\(cache.path)\""
+  ]
+#else
+  process.executableURL = URL(fileURLWithPath: "/usr/bin/unzip")
+  process.arguments = [
+    "-qq",
+    url.withUnsafeFileSystemRepresentation { String(cString: $0!) },
+    "-d", cache.path
+  ]
+#endif
+  try process.run()
+  process.waitUntilExit()
+}
+
+let cache =
+  try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask,
+                               appropriateFor: nil, create: true)
+
+var localURL = cache
+#if os(Windows)
+  localURL.appendPathComponent("tensorflow-windows-x64.zip")
+  let resourceURL: URL = URL(string: "https://artprodeus21.artifacts.visualstudio.com/A8fd008a0-56bc-482c-ba46-67f9425510be/3133d6ab-80a8-4996-ac4f-03df25cd3224/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL2NvbXBuZXJkL3Byb2plY3RJZC8zMTMzZDZhYi04MGE4LTQ5OTYtYWM0Zi0wM2RmMjVjZDMyMjQvYnVpbGRJZC80Mzc2OC9hcnRpZmFjdE5hbWUvdGVuc29yZmxvdy13aW5kb3dzLXg2NA2/content?format=zip")!
+#elseif os(macOS)
+  localURL.appendPathComponent("tensorflow-darwin-x64.zip")
+  let resourceURL: URL = URL(string: "https://artprodeus21.artifacts.visualstudio.com/A8fd008a0-56bc-482c-ba46-67f9425510be/3133d6ab-80a8-4996-ac4f-03df25cd3224/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL2NvbXBuZXJkL3Byb2plY3RJZC8zMTMzZDZhYi04MGE4LTQ5OTYtYWM0Zi0wM2RmMjVjZDMyMjQvYnVpbGRJZC80Mzc2OC9hcnRpZmFjdE5hbWUvdGVuc29yZmxvdy1kYXJ3aW4teDY00/content?format=zip")!
+#else
+print("Linux (and other OSes) are not supported")
+#endif
+
+private func download(_ url: URL, to location: URL) throws {
+  let session: URLSession =
+      URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
+
+  let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+  let task: URLSessionTask = session.downloadTask(with: url) { url, response, error in
+    defer { semaphore.signal() }
+
+    guard let url = url else { return }
+    try? FileManager.default.moveItem(at: url, to: location)
+  }
+  task.resume()
+  semaphore.wait()
+}
+
+try! download(resourceURL, to: localURL)
+try! extract(localURL, cache)
+
+private func X10Flags(_ root: URL)
+    -> (HeaderSearchPath: String, LinkerSearchPath: String) {
+  return root.withUnsafeFileSystemRepresentation {
+    let root: String = String(cString: $0!)
+#if os(Windows)
+    return (HeaderSearchPath: "\(root)\\tensorflow-windows-x64\\Library\\tensorflow-2.3.0\\usr\\include",
+            LinkerSearchPath: "\(root)\\tensorflow-windows-x64\\Library\\tensorflow-2.3.0\\usr\\lib")
+#else
+    return (HeaderSearchPath: "\(root)/tensorflow-darwin-x64/Library/tensorflow-2.3.0/usr/include",
+            LinkerSearchPath: "\(root)/tensorflow-darwin-x64/Library/tensorflow-2.3.0/usr/lib")
+#endif
+  }
+}
+
+let X10 = X10Flags(cache)
 let package = Package(
   name: "TensorFlow",
   platforms: [
@@ -42,7 +121,17 @@ let package = Package(
       dependencies: []),
     .target(
       name: "CTensorFlow",
-      dependencies: []),
+      dependencies: [],
+      cSettings: [
+        .unsafeFlags([
+          "-I\(X10.HeaderSearchPath)",
+        ])
+      ],
+      linkerSettings: [
+        .unsafeFlags([
+          "-L\(X10.LinkerSearchPath)"
+        ]),
+      ]),
     .target(
       name: "TensorFlow",
       dependencies: [
